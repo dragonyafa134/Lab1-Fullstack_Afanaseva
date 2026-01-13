ea2a479cb64f93e5fbe4f03eb84cf13d905b1175
Daria <dasha.afanaseva.1206@mail.ru>
2026-01-13 19:14:54 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Афанасьева ДН");
+Console.WriteLine(DateTime.Now);
+
